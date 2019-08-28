334545f918f6c92f1bf8c06e6aeb93eb317b8569
Nurdaulet <nurik9293709@gmail.com>
2019-08-28 12:08:59 +0600

    config sync

--- src/config/apie.php
+++ src/config/apie.php
@@ -2,10 +2,10 @@
 
 return [
     'route_prefix' => env('APIE_PREFIX', 'apie'),
+    'documentation_route' => env('APIE_DOCUMENTATION', ''),
 
     'default_level' => env('APIE_DEFAULT_LEVEL', 's'),
 
     'models_path' => env('APIE_MODELS_PATH', 'App\\'),
-
-    'models' => ['Product', 'Category', 'Brand', 'Review', 'Stock'],
+    'models' => [],
 ];
